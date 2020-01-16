75ea899766bc974ef974979b93d169ed26833ed5
neox56 <neox56@gmail.com>
2020-01-16 11:53:17 +0400

    Создана стуктура приложения. Реализованы компоненты apiConfig, apiService

---
+++ src/js/services/apiService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import config from '../config/apiConfig';
+
+/**
+ *  / countries - array of countries
+ *  / citits - array of cities
+ * /prices/cheap  - array
+ */
+class Api {
+    constructor(config) {
+        this.url = config.url
+    }
+    async countries() {
+        try {
+            const response = await axios.get(`${this.url}/countries`);
+            return response.data;
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    async cities() {
+        try {
+            const response = await axios.get(`${this.url}/cities`);
+            return response.data;
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+const api = new Api(config);
+export default api;
